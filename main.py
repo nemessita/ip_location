60473ca3c135b99923dc2f7e300ba9e4d82ca449
Ismayil Dadashov <141844064+nemessita@users.noreply.github.com>
2024-04-22 09:38:07 +0400

    test

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from pyfiglet import Figlet
+import folium
+
+def get_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(f'http://ip-api.com/json/{ip}').json()
+        # data = response.json()
+        # print(data)
+
+        data = {
+            '[IP]': response.get('query'),
+            '[Int prov]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Country]': response.get('country'),
+            '[Region Name]': response.get('regionName'),
+            '[City]': response.get('city'),
+            '[ZIP]': response.get('zip'),
+            '[Lat]': response.get('lat'),
+            '[Lon]': response.get('lon'),
+            
+        }
+        for k, v in data.items():
+            print(f'{k} : {v}')
+        area = folium.Map(location=[response.get('lat'), response.get('lon'),])
+        area.save(f'{response.get("query")}_{response.get("city")}.html')
+
+
+    except requests.exceptions.ConnectionError:
+        print('[!] Please check connecting')
+
+def main():
+    preview_text = Figlet(font='slant')
+    print(preview_text.renderText('IP INFO'))
+    ip = input('Enter IP: ')
+    get_ip(ip)
+
+if __name__ == '__main__':
+    main()
